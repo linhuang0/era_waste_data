4d5603b2b0a167f03222c24f811368a4238520b2
LINHUANG0 <120541669+LINHUANG0@users.noreply.github.com>
2023-12-10 21:04:50 +1300

    Update models.py

--- suppliers/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class CUSTOMERS(models.Model):
-    customer_name = models.CharField(max_length=128)
-    customer_number = models.CharField(max_length=32, unique=True)
-
-    def __str__(self):
-        return self.customer_name
